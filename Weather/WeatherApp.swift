2effd6107c9c2eda94baf388adaf98d5f28a13ba
chai <chaithawat.contact@gmail.com>
2024-09-03 23:01:37 +0700

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Chaithawat Pinsuwan on 3/9/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
